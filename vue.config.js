b2eadac302bb9a8a67b8241c2fe86e5b9333688f
jean-philippe.bazonnais <jean-philippe.bazonnais@ign.fr>
2021-04-16 21:14:51 +0200

    change route for production !

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? 
-  "/geoportal-extensions/" : "",
+  "/geoportal-extensions-openlayers-samples/" : "",
   configureWebpack: config => {},
   chainWebpack: config => {},
 }
